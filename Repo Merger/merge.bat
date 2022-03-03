1247f924956b55127604bfd1eabe24ba3c5bc09b
William Hollywood <will@oceanplait.com>
2022-03-03 19:38:23 +1300

    tweaking folder copying

---
+++ Repo Merger/merge.bat
@@ -0,0 +1,26 @@
+echo on
+set repotomerge=%1
+shift
+set name=%1
+shift
+set url=%1
+shift
+set srcbranch=%1
+shift
+set oldcd=%CD%
+
+cd %repotomerge%
+
+git remote add %name% %url%
+
+git fetch %name%
+
+if "%srcbranch%"=="" (git checkout remotes/%name%/master) else (git checkout remotes/%name%/%srcbranch%)
+
+set temp="..\%name%"
+
+mkdir %temp%
+
+move *.* %temp%
+
+move %temp% .\
